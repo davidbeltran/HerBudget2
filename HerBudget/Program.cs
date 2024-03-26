705fd5ba58f7f2e1ea52d390e110f71e584591f0
David Beltran <beltrannowd5@gmail.com>
2024-03-26 16:20:15 -0700

    found pdf solution. implemented Regex solution

--- HerBudget/Program.cs
+++ HerBudget/Program.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Text.RegularExpressions;
 using UglyToad.PdfPig;
 using UglyToad.PdfPig.Content;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
 
 namespace HerBudget
 {
@@ -9,32 +10,39 @@ namespace HerBudget
     {
         static void Main(string[] args)
         {
+            string pageText = "";
+            string pattern = "(?:\\n((?:0[1-9]|1[1,2])/(?:0[1-9]|[12][0-9]|3[01]))\\s*(.+) ((?:-\\d+\\.\\d{2})|(?:\\d+\\.\\d{2})))";
+            string pathTxt = "D:/afterGrad/c#/Adelisa/HerBudget/NovDec.txt";
+            string pathPdf = "D:/afterGrad/c#/Adelisa/HerBudget/NovDec.pdf";
+            ArrayList expList = new ArrayList();
 
-            using (PdfDocument document = PdfDocument.Open("D:/afterGrad/c#/Adelisa/HerBudget/NovDec.pdf"))
+            using (PdfDocument document = PdfDocument.Open(pathPdf))
             {
-                string pageText = "";
-                string pattern = "(?:\\n((?:0[1-9]|1[1,2])/(?:0[1-9]|[12][0-9]|3[01]))\\s*(.+) ((?:-\\d+\\.\\d{2})|(?:\\d+\\.\\d{2})))";
-                string path = "D:/afterGrad/c#/Adelisa/HerBudget/NovDec.txt";
-                Page page = document.GetPage(3);
-                
-                //foreach (Page page in document.GetPages())
-                //{
-                //    foreach(Word word in page.GetWords())
-                //    {
-                //        pageText += word;
-                //    }
 
-                //}
-
-                foreach(Word word in page.GetWords())
+                foreach(Page page in document.GetPages())
                 {
-                    pageText += word.ToString();
+                    var text = ContentOrderTextExtractor.GetText(page);
+                    pageText += text;
                 }
-
-                File.WriteAllText(path, pageText);
+                //File.WriteAllText(pathTxt, pageText);
             }
 
-                WebStarter ws = new WebStarter(args);
+            MatchCollection matches = Regex.Matches(pageText, pattern);
+            foreach (Match match in matches) 
+            {
+                var temp = new ArrayList();
+                temp.Add(match.Groups[1].Value);
+                temp.Add(match.Groups[2].Value);
+                temp.Add(double.Parse(match.Groups[3].Value));
+                expList.Add(temp);
+            }
+
+            foreach (ArrayList expense in expList)
+            {
+                Console.WriteLine("Date: {0}, Detail: {1}, Amount:{2}", expense[0], expense[1], expense[2]);
+            }
+
+            WebStarter ws = new WebStarter(args);
             ws.ShowWeb();
         }
     }
